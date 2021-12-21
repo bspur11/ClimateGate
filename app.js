cc5b8404c2928d9431aca905e715668dc90c957e
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-12-20 21:13:15 -0500

    first parse

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 async function getData() {
-const response = await fetch('test.csv');
-const data = await response.text();
-console.log(data);
+  const response = await fetch('ZonAnn.Ts+dSST.csv');
+  const data = await response.text();
+
+
+  const rows = data.split('\n').slice(1);
+  rows.forEach(elt => {
+    const columns = elt.split(',');
+    const year = columns[0];
+    const globTemp = columns[1];
+    console.log(year, globTemp);
+   
+  })
 }
 
 getData();
