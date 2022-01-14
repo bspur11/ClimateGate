3980a8ba0c87e56e69836cb35c7244e11ef126ca
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2022-01-13 19:20:26 -0500

    Line chart Good

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
+chartIt();
+
+async function chartIt() {
+  const data =await getData();
+  const ctx = document.getElementById('chart').getContext('2d');
+
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: data.xs,
+      datasets: [{
+        label: 'Combined Land-Surface Air and Sea-Surface Water Temperature in C°',
+        data: data.ys,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false,
+
+          ticks: {
+            // Include a dollar sign in the ticks
+            callback: function(value, index, ticks) {
+                return  value + 'C°';
+            }
+          }
+        }
+      }
+    }
+  });
+}
+
 async function getData() {
+
+  const xs = [];
+  const ys = [];
+
   const response = await fetch('ZonAnn.Ts+dSST.csv');
   const data = await response.text();
 
@@ -7,10 +46,12 @@ async function getData() {
   rows.forEach(elt => {
     const columns = elt.split(',');
     const year = columns[0];
+    xs.push(year);
     const globTemp = columns[1];
+    ys.push(parseFloat(globTemp) + 14);
     console.log(year, globTemp);
-   
-  })
+  });
+  return {xs, ys };
 }
 
-getData();
+;
