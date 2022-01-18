cf265abe2e712d3ca7c41419d13da539143ebd98
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2022-01-17 19:47:06 -0500

    multi bar graph

--- app.js
+++ app.js
@@ -1,30 +1,66 @@
 chartIt();
 
 async function chartIt() {
-  const data =await getData();
+  const data = await getData();
   const ctx = document.getElementById('chart').getContext('2d');
 
-  const myChart = new Chart(ctx, {
-    type: 'line',
+  const chart = new Chart(ctx, {
+    type: 'bar',
     data: {
       labels: data.xs,
       datasets: [{
-        label: 'Combined Land-Surface Air and Sea-Surface Water Temperature in C°',
-        data: data.ys,
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-        borderColor: 'rgba(255, 99, 132, 1)',
+          label: 'Total Ink Usage in g.',
+          data: data.ys,
+          backgroundColor: 'rgb(79, 108, 204)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1
+        },
+
+        {
+          label: 'Magenta',
+          data: data.ms,
+          backgroundColor: 'magenta',
+          borderColor: 'black',
+          borderWidth: 1
+        },
+
+       { 
+        label: 'Cyan', 
+        data: data.cs,
+        backgroundColor: 'cyan',
+        borderColor: 'black',
         borderWidth: 1
-      }]
+
+        },
+        {
+          label: 'Yellow',
+          data: data.yl,
+          backgroundColor: 'darkorange',
+          borderWidth: 'black',
+          borderWidth: 1
+        },
+        {
+          label: 'Black',
+        data: data.bl,
+        backgroundColor: 'black',
+        borderWidth: 'black',
+        borderWidth: 1
+      
+      }
+
+        
+      ]
+
     },
     options: {
       scales: {
         y: {
-          beginAtZero: false,
+          beginAtZero: true,
 
           ticks: {
-            // Include a dollar sign in the ticks
-            callback: function(value, index, ticks) {
-                return  value + 'C°';
+            // Include a C° sign in the ticks
+            callback: function (value, index, ticks) {
+              return value + 'mg';
             }
           }
         }
@@ -37,21 +73,49 @@ async function getData() {
 
   const xs = [];
   const ys = [];
+  const ms = [];
+  const cs = [];
+  const yl = [];
+  const bl = [];
+  console.log(xs, ys, ms, cs, yl, bl);
 
-  const response = await fetch('ZonAnn.Ts+dSST.csv');
+
+  const response = await fetch('test.csv');
   const data = await response.text();
 
 
-  const rows = data.split('\n').slice(1);
+  const rows = data.split('\n').splice(0);
   rows.forEach(elt => {
     const columns = elt.split(',');
-    const year = columns[0];
-    xs.push(year);
-    const globTemp = columns[1];
-    ys.push(parseFloat(globTemp) + 14);
-    console.log(year, globTemp);
+    const jobNum = columns[0];
+    const inkUse = columns[30];
+    const magInk = columns[32];
+    const cyanInk = columns[33];
+    const yellInk = columns[34];
+    const blkInk =columns[35];
+    // console.log(cyanInk);
+    // console.log(inkUse);
+    xs.push(parseFloat(jobNum));
+    // const inkUse = columns[31];
+    // ys.push(parseFloat(inkUse) + 14);
+    ys.push(parseFloat(inkUse));
+    ms.push(parseFloat(magInk));
+    cs.push(parseFloat(cyanInk));
+    yl.push(parseFloat(yellInk));
+    bl.push(parseFloat(blkInk));
+
+
   });
-  return {xs, ys };
+  return {
+    xs,
+    ys,
+    ms,
+    cs,
+    yl,
+    bl
+  };
+
 }
+
 
 ;
